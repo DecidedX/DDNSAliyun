3f47bdbb6bd6df72f39b62c4264352792e34edd6
DecidedX <shang_xinw@qq.com>
2024-01-18 18:35:23 +0800

    #0004
    Build config.py

---
+++ utils/config.py
@@ -0,0 +1,23 @@
+from configparser import ConfigParser
+import os
+
+
+class Config:
+    def __init__(self):
+        self.file = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/config.ini"
+        self.config = ConfigParser()
+        self.config.read(self.file)
+        if "DDNS" not in self.config.sections():
+            self.config["DDNS"] = {
+                "access_key_id": "",
+                "access_key_secret": "",
+                "domain_name": "example.com",
+                "rrkey_word": "www",
+                "rtype": "AAAA",
+                "interval": 1
+            }
+            with open(self.file, 'w') as configfile:
+                self.config.write(configfile)
+
+    def get_ddns_config(self):
+        return self.config["DDNS"]
